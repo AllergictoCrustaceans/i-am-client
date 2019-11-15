810e8ff85dad15754ed1d0544994cd88e5a74ffa
Katie Wu <katie.wy.wu@gmail.com>
2019-11-15 09:23:55 -0800

    Create a loading flag for our login state.

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -6,6 +6,7 @@ import "./Login.css";
 export default function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
@@ -13,13 +14,14 @@ export default function Login(props) {
 
   async function handleSubmit(event) {
     event.preventDefault();
-
+    setIsLoading(true);
     try {
         await Auth.signIn(email, password);
         props.userHasAuthenticated(true);
         props.history.push('/');
     } catch(e) {
         alert(e.message);
+        setIsLoading(false);
     }
   }
 
