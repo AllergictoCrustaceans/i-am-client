3c495dd700c0843b82894f7decc84ecdbb2b6e10
Katie Wu <katie.wy.wu@gmail.com>
2019-11-15 22:11:36 -0800

    Create Main.js with buttons to endpoints.

---
+++ src/containers/Main.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {LinkContainer} from 'react-router-bootstrap';
+import "./Main.css";
+// import blender background image (INSIDE)
+
+export default function Home() {
+  return (
+    <div className="Main">
+      <div className="lander">
+        <h1>I AM </h1>
+        <div className="miaDiv">
+          <LinkContainer to="/mia">
+              <button type="button" className="btn btn-primary btn-circle btn-xl">Mia</button>
+          </LinkContainer>
+        </div>
+        <div className="messagesDiv">
+          <LinkContainer to='/messages'>
+              <button type="button" className="btn btn-primary btn-circle btn-xl">Messages</button>
+          </LinkContainer>
+        </div>
+        <div className="moodsDiv">
+          <LinkContainer to='/moods'>
+              <button type="button" className="btn btn-primary btn-circle btn-xl">Moods</button>
+          </LinkContainer>
+        </div>
+      </div>
+    </div>
+  );
+}
