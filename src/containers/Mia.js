cdc92388580921b57d70e499ce417e03b9ca53dc
Katie Wu <katie.wy.wu@gmail.com>
2019-11-18 09:34:01 -0800

    Comment out code attempted to integrate Lex bot into /mia endpoint.

--- src/containers/Mia.js
+++ src/containers/Mia.js
@@ -1,10 +1,63 @@
-import React, {useState} from 'react';
+import React, {Component, useState} from 'react'; //useState
 import {FormGroup, FormControl} from 'react-bootstrap';
-import {API} from 'aws-amplify';
+import Amplify, {Interactions, API} from 'aws-amplify'; //Interactions API
+import {ChatBot, AmplifyTheme} from 'aws-amplify-react';
 import LoaderButton from '../components/LoaderButton';
 import './Mia.css';
+// import config from '../aws-exports';
 
-export default function Mia(props) {
+// Amplify.configure ({config});
+
+// const myTheme = {
+//     ...AmplifyTheme,
+//     sectionHeader: {
+//         ...AmplifyTheme.sectionHeader,
+//         backgorundColor: '#ff6600'
+//     }
+// };
+
+// let userInput = "testing";
+// const response = Interactions.send('IAM', userInput);
+// console.log(response.message);
+
+export default function Mia(props) { // use Component when going back to bot
+    
+    // let userInput = "Testing";
+    // const response = await Interactions.send('IAM', userInput);
+    // console.log(response.message);
+    // constructor(props) {
+    //     super(props);
+    //     this.handleComplete = this.handleComplete.bind(this);
+    // }
+
+    // handleComplete(e, confirmation) {
+    //     if(e) {
+    //         console.log(e);
+    //         return;
+    //     } else {
+    //         alert('Success:' + JSON.stringify(confirmation, null, 2));
+    //         return 'So we can talk now, huh? Sweet.';
+    //     }
+        
+    // }
+    // render() {
+    //     return (
+    //         <div className="App">
+    //             <header className = "App=header" >
+    //                 <h3 className = "App-title">Please greet Mia to start a conversation</h3>
+    //             </header>
+    //             <ChatBot 
+    //                 title="MIA"
+    //                 theme={myTheme}
+    //                 botName="IAM_test"
+    //                 // onComplete={this.handleComplete.bind(this)}
+    //                 clearOnComplete={true}
+    //                 conversationModeOn={false}
+    //             />
+    //         </div>
+    //     )
+    // }
+
     const [content, setContent] = useState('');
     const [isLoading, setIsLoading] = useState(false);
 
