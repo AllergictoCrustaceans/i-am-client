bf8c3e98b6aea3632d47280d1bf932615fd98fec
Katie Wu <katie.wy.wu@gmail.com>
2019-11-18 22:04:00 -0800

    Create Route for endpoint messages/id.

--- src/Routes.js
+++ src/Routes.js
@@ -8,16 +8,18 @@ import Signup from './containers/Signup';
 import Main from './containers/Main';
 import Mia from './containers/Mia';
 import Messages from './containers/Messages';
+import Mood from './containers/Mood';
 
 export default function Routes({appProps}) {
   return (
     <Switch>
-        <AppliedRoute path='/' exact component={Home} appProps={appProps} />
-        <AppliedRoute path='/login' exact component={Login} appProps={appProps}/>
+        <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+        <AppliedRoute path="/login" exact component={Login} appProps={appProps}/>
         <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
         <AppliedRoute path='/main' expact component={Main} appProps={appProps} />
         <AppliedRoute path="/mia" exact component={Mia} appProps={appProps} />
         <AppliedRoute path="/messages" exact component={Messages} appProps={appProps} />
+        <AppliedRoute path="/messages/id" exact component={Mood} appProps={appProps} />
         <Route component={NotFound} />
     </Switch>
   );
