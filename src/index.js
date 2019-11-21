b6aea0736b46fceeb8b79021a7640767fa5cb8b6
Katie Wu <katie.wy.wu@gmail.com>
2019-11-20 21:31:30 -0800

    Change endpoint name mia to moods.

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ Amplify.configure({
     API: {
       endpoints: [
         {
-          name: "mia",
+          name: "moods",
           endpoint: config.apiGateway.URL,
           region: config.apiGateway.REGION
         },
