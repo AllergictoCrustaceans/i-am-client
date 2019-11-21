2f44e3f99543b35d6fde860c976f7ff653764423
Katie Wu <katie.wy.wu@gmail.com>
2019-11-20 21:32:27 -0800

    Create a useState and setUser hook for sub and email when logged in.

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from './containers/Home';
 import Login from './containers/Login';
@@ -11,15 +11,23 @@ import Messages from './containers/Messages';
 import Mood from './containers/Mood';
 
 export default function Routes({appProps}) {
+  const [sub, setSub] = useState();
+  const [email, setEmail] = useState();
+
+  const setUser = (subFromLogin, emailFromLogin) => {
+    setSub(subFromLogin);
+     setEmail(emailFromLogin);
+  }
+
   return (
     <Switch>
         <AppliedRoute path="/" exact component={Home} appProps={appProps} />
-        <AppliedRoute path="/login" exact component={Login} appProps={appProps}/>
+        <AppliedRoute path="/login" exact component={Login} appProps={{...appProps, setUser}}/>
         <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
         <AppliedRoute path='/main' expact component={Main} appProps={appProps} />
-        <AppliedRoute path="/mia" exact component={Mia} appProps={appProps} />
+        <AppliedRoute path="/mia" exact component={Mia} appProps={{...appProps, sub, email}} />
         <AppliedRoute path="/messages" exact component={Messages} appProps={appProps} />
-        <AppliedRoute path="/messages/id" exact component={Mood} appProps={appProps} />
+        <AppliedRoute path="/moods" exact component={Mood} appProps={{...appProps, sub, email}} />
         <Route component={NotFound} />
     </Switch>
   );
