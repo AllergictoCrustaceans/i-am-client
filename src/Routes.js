96721aca932b2df056b01bdda17bddef4b131dcb
Katie Wu <katie.wy.wu@gmail.com>
2019-11-22 10:56:55 -0800

    Comment out non-working component Messages.js

--- src/Routes.js
+++ src/Routes.js
@@ -7,7 +7,7 @@ import AppliedRoute from './components/AppliedRoute';
 import Signup from './containers/Signup';
 import Main from './containers/Main';
 import Mia from './containers/Mia';
-import Messages from './containers/Messages';
+// import Messages from './containers/Messages';
 import Mood from './containers/Mood';
 
 export default function Routes({appProps}) {
@@ -23,10 +23,10 @@ export default function Routes({appProps}) {
     <Switch>
         <AppliedRoute path="/" exact component={Home} appProps={appProps} />
         <AppliedRoute path="/login" exact component={Login} appProps={{...appProps, setUser}}/>
-        <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
+        <AppliedRoute path="/signup" exact component={Signup} appProps={{...appProps, setUser}} />
         <AppliedRoute path='/main' expact component={Main} appProps={appProps} />
         <AppliedRoute path="/mia" exact component={Mia} appProps={{...appProps, sub, email}} />
-        <AppliedRoute path="/messages" exact component={Messages} appProps={appProps} />
+        {/* <AppliedRoute path="/messages" exact component={Messages} appProps={appProps} /> */}
         <AppliedRoute path="/moods" exact component={Mood} appProps={{...appProps, sub, email}} />
         <Route component={NotFound} />
     </Switch>
